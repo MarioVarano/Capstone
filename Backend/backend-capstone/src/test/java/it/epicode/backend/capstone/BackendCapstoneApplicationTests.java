aa28d7318437ff04a996cf74becec18ae168ec3f
MarioVarano <alexxan@live.it>
2024-06-21 11:02:48 +0200

    capstone

---
+++ Backend/backend-capstone/src/test/java/it/epicode/backend/capstone/BackendCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.backend.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
